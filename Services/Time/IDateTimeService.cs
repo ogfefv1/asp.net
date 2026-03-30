b8a4b7d6530d8f140cda9ace62ca5dc53c015b99
Yehor Dzhemlikhanov <ogfefv@gmail.com>
2026-03-30 21:09:31 +0300

    .

---
+++ Services/Time/IDateTimeService.cs
@@ -0,0 +1,10 @@
+﻿namespace AspKnP231.Services.Storage
+{
+    public static class StorageExtension
+    {
+        public static IServiceCollection AddStorage(this IServiceCollection services)
+        {
+            return services.AddSingleton<IStorageService, LocalStorageService>();
+        }
+    }
+}
